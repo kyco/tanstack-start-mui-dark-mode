a6793ca6f417aa1f14ad0aae7734be0bc132cb75
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-03-11 14:09:33 +0100

    feat: Base MUI install

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 This project was started with [TanStack Start - Bare Example](https://github.com/TanStack/router/tree/main/examples/react/start-bare).
 
+MUI was installed via the [default installation](https://mui.com/material-ui/getting-started/installation/#default-installation).
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router'
+import Button from '@mui/material/Button';
+
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+
   return (
     <main>
       <h1>Hello world!</h1>
+      <Button variant="contained">Hello world</Button>
     </main>
   )
 }
